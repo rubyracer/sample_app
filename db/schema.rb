3ed2bbaaf34a61b4cc257b6579c444b55afc8be0
rubyracer <occracing@yandex.ru>
2020-01-07 14:17:05 +0600

    add index to UsersEmail

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_01_06_090702) do
+ActiveRecord::Schema.define(version: 2020_01_07_070039) do
 
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
 end
