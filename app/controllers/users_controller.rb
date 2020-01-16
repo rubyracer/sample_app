f53cefb6efae131c27d54e1adeac0a25bb7b0770
rubyracer <occracing@yandex.ru>
2020-01-16 14:51:37 +0600

    add pagination test

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,7 +4,8 @@ class UsersController < ApplicationController
 
   def index
     # @users = User.all
-    @users = User.paginate(page: params[:page])
+    # @users = User.paginate(page: params[:page])
+    @users = User.paginate(page: params[:page], per_page: 9)
   end
 
   def show
