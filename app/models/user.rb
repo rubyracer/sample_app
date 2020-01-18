366279cf1a10fbdeace20f9e9c1d60e995b1ae7d
rubyracer <occracing@yandex.ru>
2020-01-18 14:05:56 +0600

    use account activations controller

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,9 @@
 class User < ApplicationRecord
-  attr_accessor :remember_token
-
+  attr_accessor :remember_token, :activation_token
   # before_save { self.email = email.downcase }
-  before_save { email.downcase! }
+  # before_save { email.downcase! }
+  before_save   :downcase_email
+  before_create :create_activation_digest
 
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\- ]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
 
@@ -49,4 +50,17 @@ class User < ApplicationRecord
   def forget
     update_attribute(:remember_digest, nil)
   end
+
+  private
+
+    # Переводит адрес электронной почты в нижний регистр.
+    def downcase_email
+      self.email = email.downcase
+    end
+
+    # Создает и присваивает активационнй токен и дайджест.
+    def create_activation_digest
+      self.activation_token  = User.new_token
+      self.activation_digest = User.digest(activation_token)
+    end
 end
