d84de943826d85e72ae54f00890e58cf65a2cf33
rubyracer <occracing@yandex.ru>
2020-01-20 12:46:28 +0600

    add password_resets_controller

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-  attr_accessor :remember_token, :activation_token
+  attr_accessor :remember_token, :activation_token, :reset_token
   # before_save { self.email = email.downcase }
   # before_save { email.downcase! }
   before_save   :downcase_email
@@ -68,6 +68,18 @@ class User < ApplicationRecord
   def activate
     update_attribute(:activated,    true)
     update_attribute(:activated_at, Time.zone.now)
+  end
+
+  # Устанавливает атрибуты для сброса пароля.
+  def create_reset_digest
+    self.reset_token = User.new_token
+    update_attribute(:reset_digest,  User.digest(reset_token))
+    update_attribute(:reset_sent_at, Time.zone.now)
+  end
+
+  # Отправляет электронное письмо для сброса пароля.
+  def send_password_reset_email
+    UserMailer.password_reset(self).deliver_now
   end
 
   private
