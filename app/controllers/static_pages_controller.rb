259daffdfa613c3add77ffc6e5ab94c15cbe09d0
rubyracer <occracing@yandex.ru>
2020-01-29 13:10:07 +0600

    Create microposts

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,5 +1,7 @@
 class StaticPagesController < ApplicationController
   def home
+    # @micropost = current_user.microposts.build if current_user
+    @micropost = current_user.microposts.build if logged_in?
   end
 
   def help
