b7eaf9466595377aed13c5b5a9f5a372d561d2a3
rubyracer <occracing@yandex.ru>
2020-01-29 11:08:55 +0600

    Add microposts

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,6 +13,8 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
     redirect_to root_url and return unless @user.activated?
+    # @microposts = @user.microposts.paginate(page: params[:page])
+    @microposts = @user.microposts.paginate(page: params[:page], per_page: 9)
     # debugger
   end
 
