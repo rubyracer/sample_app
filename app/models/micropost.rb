4910f11f7bc5e40407a686972d6835ac29f1ae46
rubyracer <occracing@yandex.ru>
2020-01-31 12:22:50 +0600

    Images validation

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -4,4 +4,14 @@ class Micropost < ApplicationRecord
   mount_uploader :picture, PictureUploader
   validates :user_id, presence: true
   validates :content, presence: true, length: { maximum: 140 }
+  validate :picture_size
+
+  private
+
+  # Проводит валидацию размера загруженного изображения.
+  def picture_size
+    if picture.size > 5.megabytes
+      errors.add(:picture, "Should be less than 5MB")
+    end
+  end
 end
--- app/uploaders/picture_uploader.rb
+++ app/uploaders/picture_uploader.rb
@@ -12,6 +12,8 @@ class PictureUploader < CarrierWave::Uploader::Base
 
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
+  # Указывает каталог для хранения загруженных файлов.
+  # Разумно оставить значение по умолчанию загрузчикам для монтирования.
   def store_dir
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
@@ -38,9 +40,10 @@ class PictureUploader < CarrierWave::Uploader::Base
 
   # Add a white list of extensions which are allowed to be uploaded.
   # For images you might use something like this:
-  # def extension_white_list
-  #   %w(jpg jpeg gif png)
-  # end
+  # Добавляет белый список расширений, которые позволено загружать.
+  def extension_white_list
+    %w(jpg jpeg gif png)
+  end
 
   # Override the filename of the uploaded files:
   # Avoid using model.id or version_name here, see uploader/store.rb for details.
